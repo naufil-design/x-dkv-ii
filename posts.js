c98f03f5f725e0946d14f2bc80953fae6f32f0eb
Naufil Design <naufilefendiii@gmail.com>
2025-09-16 22:18:41 +0700

    update posts.js

---
+++ posts.js
@@ -0,0 +1,20 @@
+const posts = [
+  {
+    img: "gallery/photo1.png",
+    title: "potret kelas X DKV II",
+    desc: "pemandangan kelas X DKV II saat membaca doa di pagi hari.",
+    link: "gallery/photo1.png"
+  },
+  {
+    img: "gallery/photo2.png",
+    title: "pemenang lomba poster",
+    desc: "- Muhammad Naufil Efendi (pemenang kategori memorable terbaik)<br>- Nafiza Agustin (pemenang kategori karya asli terbaik)",
+    link: "gallery/photo2.png"
+  },
+  {
+    img: "gallery/photo3.png",
+    title: "mengikuti lomba mural",
+    desc: "Muhammad Naufil Efendi direkrut untuk mengikuti lomba mural <i>SMANJA ART COMPETITION 2025</i>.",
+    link: "gallery/photo3.png"
+  }
+];
