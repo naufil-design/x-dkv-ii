cf2d6fff15ed86cb03fdd728b696619e6e504902
Naufil Design <naufilefendiii@gmail.com>
2025-09-16 22:21:11 +0700

    update posts.js

--- posts.js
+++ posts.js
@@ -1,9 +1,9 @@
 const posts = [
   {
-    img: "gallery/photo1.png",
-    title: "potret kelas X DKV II",
-    desc: "pemandangan kelas X DKV II saat membaca doa di pagi hari.",
-    link: "gallery/photo1.png"
+    img: "gallery/photo3.png",
+    title: "mengikuti lomba mural",
+    desc: "Muhammad Naufil Efendi direkrut untuk mengikuti lomba mural <i>SMANJA ART COMPETITION 2025</i>.",
+    link: "gallery/photo3.png"
   },
   {
     img: "gallery/photo2.png",
@@ -12,9 +12,9 @@ const posts = [
     link: "gallery/photo2.png"
   },
   {
-    img: "gallery/photo3.png",
-    title: "mengikuti lomba mural",
-    desc: "Muhammad Naufil Efendi direkrut untuk mengikuti lomba mural <i>SMANJA ART COMPETITION 2025</i>.",
-    link: "gallery/photo3.png"
+    img: "gallery/photo1.png",
+    title: "potret kelas X DKV II",
+    desc: "pemandangan kelas X DKV II saat membaca doa di pagi hari.",
+    link: "gallery/photo1.png"
   }
 ];
